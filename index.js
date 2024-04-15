16745a3d51d04b00437d116716114ffc90b98bf3
Nick Drian <nickdrian@Nicks-MacBook-Air.local>
2024-04-15 00:57:00 -0600

    working stack

---
+++ index.js
@@ -0,0 +1,18 @@
+import { greet, code_that_throws, run } from './pkg';
+
+let res = greet('World');
+console.log(res);
+
+res = code_that_throws();
+console.log(res);
+
+
+var canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+
+var div = document.createElement('div');
+document.body.appendChild(div);
+div.id = "wasm-example";
+
+await run();
+console.log("ran");

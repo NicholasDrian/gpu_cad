063be3088c043a2609e8169dce4f52500057f1f4
Nick Drian <nickdrian@Nicks-MacBook-Air.local>
2024-04-19 00:17:05 -0600

    setup testing

--- src/lib.rs
+++ src/lib.rs
@@ -15,15 +15,6 @@ use winit::{
     window::WindowBuilder,
 };
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
-
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
--- tests/math/vec3.rs
+++ tests/math/vec3.rs
@@ -1,2 +1,21 @@
+use wasm_bindgen_test::*;
 
+wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
+#[wasm_bindgen_test]
+pub fn correct_function() {
+    let x = 2;
+    assert_eq!(2, x);
+}
+
+#[wasm_bindgen_test]
+pub fn correct_function1() {
+    let x = 2;
+    assert_eq!(2, x);
+}
+
+#[wasm_bindgen_test]
+pub fn correct_function2() {
+    let x = 2;
+    assert_eq!(2, x);
+}
